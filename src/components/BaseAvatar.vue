c69c99aae75b5c927965a03e3de9f89dbad20274
Evan Sparkman <me@evansparkman.com>
2018-08-28 11:28:24 -0400

    Stub in Repo Data to verify components render as expected

---
+++ src/components/BaseAvatar.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <img src="https://en.gravatar.com/userimage/3498054/6dfe6a2dc8306c09abfe90a355cbf808.jpeg">
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+img {
+  border-radius: 50%;
+}
+</style>
