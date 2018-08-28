a9c6cb025ba4fff27468b2f1b707c98ce0c9d89f
Evan Sparkman <me@evansparkman.com>
2018-08-28 10:28:07 -0400

    Rename HelloWorld Component to RepoCard. Update references. Add prettier config

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,18 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <RepoCard msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import RepoCard from "@/components/RepoCard.vue";
 
 export default {
   name: "home",
   components: {
-    HelloWorld
+    RepoCard
   }
 };
 </script>
